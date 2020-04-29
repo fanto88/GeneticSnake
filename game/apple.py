7ee3a08e2711a148d31b8c15ad0f3b7bf784d311
Davide Fantini <fantinidavide9@gmail.com>
2020-04-29 16:57:27 +0200

    Created Apple

---
+++ game/apple.py
@@ -0,0 +1,22 @@
+from utils import config
+
+
+class Apple:
+    """Class that represent the Apple inside of game."""
+    def __init__(self, position):
+        self.__position = position
+        self.__points = config.APPLE_POINTS
+
+    def position(self, position=None):
+        """If position is none then return Apple position, otherwise assign a new value to Apple position."""
+        if position is None:
+            return self.__position
+        self.__position = position
+        return self
+
+    def points(self, points=None):
+        """If points is none then return Apple points, otherwise assign a new value to Apple points."""
+        if points is None:
+            return self.__points
+        self.__points = points
+        return self
