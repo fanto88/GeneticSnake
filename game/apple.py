2cd6cb55e59850cc210954f508c34f7494508de7
Davide Fantini <fantinidavide9@gmail.com>
2020-04-29 17:01:27 +0200

    Code Refactor

--- game/apple.py
+++ game/apple.py
@@ -3,6 +3,7 @@ from utils import config
 
 class Apple:
     """Class that represent the Apple inside of game."""
+
     def __init__(self, position):
         self.__position = position
         self.__points = config.APPLE_POINTS
