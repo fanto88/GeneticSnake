ec0c9469501bd7b9f1a0450bae05e1d2ee004ae5
Davide Fantini <fantinidavide9@gmail.com>
2020-04-30 14:09:06 +0200

    Apple code cleanup

--- game/apple.py
+++ game/apple.py
@@ -5,19 +5,6 @@ class Apple:
     """Class that represent the Apple inside of game."""
 
     def __init__(self, position):
-        self.__position = position
-        self.__points = config.APPLE_POINTS
+        self.position = position
+        self.points = config.APPLE_POINTS
 
-    def position(self, position=None):
-        """If position is none then return Apple position, otherwise assign a new value to Apple position."""
-        if position is None:
-            return self.__position
-        self.__position = position
-        return self
-
-    def points(self, points=None):
-        """If points is none then return Apple points, otherwise assign a new value to Apple points."""
-        if points is None:
-            return self.__points
-        self.__points = points
-        return self
