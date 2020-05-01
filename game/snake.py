60e4c65c671269292236957c679f79338d38603c
Davide Fantini <fantinidavide9@gmail.com>
2020-05-01 02:59:48 +0200

    Created Snake

---
+++ game/snake.py
@@ -0,0 +1,29 @@
+import numpy
+
+from utils import config
+
+
+class Snake:
+    """Class that represent the snake inside the game."""
+
+    def __init__(self):
+        self.position = numpy.array([
+            [(config.GRID_SIZE[0] / 2) * config.RECT_SIZE[0], (config.GRID_SIZE[1] / 2 - 2) * config.RECT_SIZE[1]],
+            [(config.GRID_SIZE[0] / 2) * config.RECT_SIZE[0], (config.GRID_SIZE[1] / 2 - 1) * config.RECT_SIZE[1]],
+            [(config.GRID_SIZE[0] / 2) * config.RECT_SIZE[0], (config.GRID_SIZE[1] / 2) * config.RECT_SIZE[1]]
+        ])
+        self.speed = numpy.array([config.RECT_SIZE[0], config.RECT_SIZE[1]])
+        self.length = 3
+
+    def move_snake(self, action):
+        """Move the snake based on the Action."""
+        for i in range(self.length - 1, 0, -1):
+            self.position[i] = self.position[i - 1]
+        self.position[0] = numpy.add(self.position[0], numpy.multiply(action, self.speed))
+
+    def add_piece(self):
+        """Add config.PIECE_FOR_APPLE new pieces to the Snake."""
+        for value in range(config.PIECE_FOR_APPLE):
+            arr = self.position[self.length - 1]
+            self.position = numpy.vstack((self.position, arr))
+            self.length += 1
